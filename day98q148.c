561cea43a9f3e288669e3c27b56f8791fca6ac93
singh01aashi-del <singh01aashi@gmail.com>
2025-11-27 23:06:05 +0530

    Add files via upload

---
+++ day98q148.c
@@ -0,0 +1,42 @@
+//Q148: Take two structs as input and check if they are identical.
+
+/*
+Sample Test Cases:
+Input 1:
+Student1: Asha 101 90
+Student2: Asha 101 90
+Output 1:
+Same
+
+*/
+#include <stdio.h>
+#include <string.h>
+
+struct Student {
+    char name[50];
+    int roll;
+    int marks;
+};
+
+int main() {
+    struct Student s1, s2;
+
+    // Input for student 1
+    printf("Enter details of Student 1 (name roll marks): ");
+    scanf("%s %d %d", s1.name, &s1.roll, &s1.marks);
+
+    // Input for student 2
+    printf("Enter details of Student 2 (name roll marks): ");
+    scanf("%s %d %d", s2.name, &s2.roll, &s2.marks);
+
+    // Compare both structures
+    if (strcmp(s1.name, s2.name) == 0 &&
+        s1.roll == s2.roll &&
+        s1.marks == s2.marks) {
+        printf("Same\n");
+    } else {
+        printf("Not Same\n");
+    }
+
+    return 0;
+}
